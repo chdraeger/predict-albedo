4ca7fae14db2855ea443704eeefd59394ea751be
chdraeger <christina.draeger@posteo.de>
2023-12-27 08:40:57 -0800

    final generator version

--- train.py
+++ train.py
@@ -9,21 +9,22 @@ import numpy as np
 import pyreadr
 
 class dataloader(Sequence):
-    def __init__(self, path, batch_size, shuffle=None, transform=None, transform_file=None):
+    def __init__(self, path, batch_size, transform=None, transform_file=None, shuffle=None):
         self.path = path
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.transform = transform
         self.transform_file = transform_file
-        self.filenames = self.get_all_files_in_path(path)
+        self.filenames = self.get_all_files_in_path(path, shuffle)
 
     def __len__(self):
         return int(np.ceil(len(self.filenames) / float(self.batch_size))) # total number of batches in the full dataset
 
-    def get_all_files_in_path(self,path):
-        # filenames =
-        # np.random.shuffle(train_file_names)
-        return glob.glob(path + '*' + '.RDS')
+    def get_all_files_in_path(self, path, shuffle):
+        filenames = glob.glob(path + '*' + '.RDS')
+        if shuffle:
+            np.random.shuffle(filenames)
+        return filenames
 
     def __getitem__(self, index): # length of sequence
         file = self.filenames[index]
@@ -49,71 +50,34 @@ def build_model():
     model.compile(loss='mse', optimizer='adam', metrics=['mae'])
     return model
 
-# fit the model
-def fit_model(epochs, batch_size, validation_split):
-    x_train, y_train = create_dataset()
+
+if __name__ == "__main__":
+    # specifications
+    epochs = 10 # 30
+    batch_size = 40000 # 2**7
+    path_train = 'data/train/'
+    path_validate = 'data/validate/'
+    transform_file = 'data/meta/std.csv'
+
+    # get data
+    train_generator = dataloader(path_train, batch_size, transform=True, transform_file=transform_file, shuffle=True)
+    validate_generator = dataloader(path_validate, batch_size, transform=True, transform_file=transform_file)
+
+    # model
     model = build_model()
     build_model().summary()
 
-    callbacks = [ModelCheckpoint(filepath='model.hdf5',
-                        monitor='val_loss',
-                        verbose=1,
-                        save_best_only=True,
-                        mode='min'),
-        CSVLogger('history.csv')
-    ]
+    callbacks = [ModelCheckpoint(filepath='output/model.hdf5', save_best_only=True),
+                 CSVLogger('output/history.csv')]
 
     fitted_model = model.fit(
-        x_train,
-        y_train,
+        train_generator,
+        validation_data=validate_generator,
         epochs=epochs,
-        batch_size=batch_size,
-        validation_split=validation_split,
         callbacks=callbacks
     )
 
     # plot
     plt = pd.DataFrame(fitted_model.history).plot(figsize=(8, 5))
     fig = plt.get_figure()
-    fig.savefig("model.png")
-
-
-if __name__ == "__main__":
-    # specifications
-    epochs = 2 # 30
-    batch_size = 40000 # 2**7
-    path_train = 'data/test1/'
-
-    # get data
-    train_generator = dataloader(path_train, batch_size, transform=True, transform_file='data/meta/std.csv')
-    for i, data in enumerate(train_generator):
-        print(i)
-
-    # fit_model(epochs, batch_size, validation_split)
-    model = build_model()
-    build_model().summary()
-
-    fitted_model = model.fit(
-        train_generator,
-        validation_data=train_generator,
-        epochs=epochs
-    )
-
-    print('here')
-
-    # dataloader_train = christinas_dataloader(path_train,transform,batch_size)
-    # for i in range(len(dataloader_train)):
-    #     data_file = dataloader_train.__getitem__(i)
-    #
-    # for data_file in dataloader_train:
-    #     niter = len(data_file)/dataloader_train.batch_size
-    #     for i in range(niter):
-    #         data = data_file[i*batch_size:(i+1)*batch_size]
-    #         input_data, output_data = data
-    #         output_prediction = model(input_data)
-    #         loss = loss_fn(output_data,output_prediction)
-    #         model.backward()
-
-    # dataloader_train.get_all_files_in_path()
-    # dataloader_train.__len__()
-    # len(dataloader_train)
+    fig.savefig("output/model.png")
